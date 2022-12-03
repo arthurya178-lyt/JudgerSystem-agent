995935df522e96a5f4014de2a9e9a8dab9cee969
李彥霆 <59197410+arthurya178-pccu@users.noreply.github.com>
2022-12-03 19:30:09 +0800

    Update setup_environment.sh

--- script/setup_environment.sh
+++ script/setup_environment.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+
 readonly BASE_ROOT=$(dirname $0)
 cd $BASE_ROOT
-
 
 source ./environment_argument.sh
 
 # clear directory
 
 # clear execute environment folder if folder exist
-rm -rf $execute_environment_path
+rm -rf $execute_path
 
 # rebuild execute environment folder
-mkdir $execute_environment_path
+mkdir $execute_path
 # change directory to execute environment
 cd $execute_environment_path
-mkdir $sourcecode_path
-mkdir $export_file_path
-mkdir $result_file_path
-
+mkdir $compile_folder
+mkdir $export_folder
+mkdir $result_folder
