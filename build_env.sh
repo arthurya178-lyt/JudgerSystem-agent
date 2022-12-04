a365367c3ef080987b56f36e3fcafe81efebcb4d
arthurya178-VM-lxc <arthurya178@gmail.com>
2022-12-04 21:43:27 +0800

    Reliability api

---
+++ build_env.sh
@@ -0,0 +1,21 @@
+sudo apt install curl git
+
+sudo apt install g++ gcc python3 openjdk-18-jre-headless openjdk-18-jdk-headless
+
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
+rm -rf ./script/execute_dir ;
+ 
+chmod 700 ./script/*.sh
+ 
+rm -rf ./script/execute_dir
+
+mkdir ./script/execute_dir
+mkdir ./script/execute_dir/compile
+mkdir ./script/execute_dir/output
+mkdir ./script/execute_dir/result
+
+npm install
+ 
+npm start
