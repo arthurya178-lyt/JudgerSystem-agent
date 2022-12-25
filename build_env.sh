0a9a9a3c29a17985bed9f0f750a2b9d985a4e13d
李彥霆 <59197410+arthurya178-pccu@users.noreply.github.com>
2022-12-25 09:42:20 +0800

    Update build_env.sh

--- build_env.sh
+++ build_env.sh
@@ -3,12 +3,12 @@ sudo apt update
 
 sudo apt install curl time
 
-sudo apt install g++ gcc python3 openjdk-18-jre-headless openjdk-18-jdk-headless
+sudo apt install g++ gcc python3 default-jre default-jdk
 
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 
-sudo apt-get install npm
+sudo apt install npm
 
 chmod 744 ./script/*.sh
  
