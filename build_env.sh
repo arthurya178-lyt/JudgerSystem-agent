184fbc77ae8fb13621d375b98f1c8e774c9fb2c3
LYT Macbook <arthurya178@gmail.com>
2023-08-31 06:45:40 +0800

    上機前整理修正

--- build_env.sh
+++ build_env.sh
@@ -1,14 +1,11 @@
 
 sudo apt update
 
-sudo apt install curl time
+sudo apt install -y curl time
 
-sudo apt install g++ gcc python3 default-jre default-jdk
+sudo apt install -y g++ gcc python3 default-jre default-jdk
 
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
-
-sudo apt install npm
+sudo apt-get install -y nodejs npm
 
 chmod 744 ./script/*.sh
  
